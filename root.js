ff9b4cc9e8b5fe2c06a3ee88b5e69c3aafe3613d
Sandilandz14 <stukani100@gmail.com>
2020-12-17 12:32:57 +0200

    Updated project files

--- root.js
+++ root.js
@@ -1,21 +1,9 @@
-/* Set the width of the side navigation to 250px 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+ /* Open the sidenav */
+ function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
   }
   
-  /* Set the width of the side navigation to 0 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";*/
-  } 
-
-  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  /* Close/hide the sidenav */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
   } 
